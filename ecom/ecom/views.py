3b40b8b77fba9577a5edba8108e888dca9de0fb6
rizkitaufiq <rizkitaufiq97@gmail.com>
2024-02-19 16:32:58 +0700

    upload project

---
+++ ecom/ecom/views.py
@@ -0,0 +1,196 @@
+from django.shortcuts import render, redirect, HttpResponse
+from app.models import Category, Product, Contact_us, Order, Brand
+from django.contrib.auth.models import User
+
+from app.models import UserCreateForm
+
+# from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib.auth.decorators import login_required
+from cart.cart import Cart
+
+def Master(request):
+    return render(request, 'master.html')
+
+def Index(request):
+    category   = Category.objects.all()
+    brand      = Brand.objects.all()
+
+    categoryID = request.GET.get('category')
+    brandID    = request.GET.get('brand')
+
+    if categoryID:
+        product     = Product.objects.filter(sub_Category=categoryID).order_by('-id')
+    elif brandID:
+        product     = Product.objects.filter(brand=brandID).order_by('-id')
+    else:
+        product     = Product.objects.all() 
+
+    context = {
+        'category' : category,
+        'product'  : product,
+        'brand'    : brand,  
+    }
+    return render(request, 'index.html', context) 
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreateForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            new_user = authenticate(
+                username = form.cleaned_data['username'],
+                password = form.cleaned_data['password1'],
+            )
+            login(request, new_user)
+            return redirect('index')
+    else:
+        form = UserCreateForm()
+
+    context = {
+        'form' : form,
+    }    
+    
+    return render(request,'registration/signup.html', context)
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
+
+
+# addtocart
+@login_required(login_url="login")
+def cart_add(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.add(product=product)
+    return redirect("index")
+
+
+@login_required(login_url="login")
+def item_clear(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.remove(product)
+    return redirect("cart_detail")
+
+
+@login_required(login_url="login")
+def item_increment(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.add(product=product)
+    return redirect("cart_detail")
+
+
+@login_required(login_url="login")
+def item_decrement(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.decrement(product=product)
+    return redirect("cart_detail")
+
+
+@login_required(login_url="login")
+def cart_clear(request):
+    cart = Cart(request)
+    cart.clear()
+    return redirect("cart_detail")
+
+
+@login_required(login_url="login")
+def cart_detail(request):
+    return render(request, 'cart/cart_detail.html')
+
+
+# contact
+def contact_page(request):
+    if request.method == "POST":
+        contact = Contact_us(
+            name    = request.POST.get('name'),
+            email   = request.POST.get('email'),
+            subject = request.POST.get('subject'),
+            message = request.POST.get('message'),
+        )
+        contact.save()
+        
+    return render(request, 'contact.html')
+
+# checkout
+def checkout_page(request):
+    if request.method == "POST":
+        address = request.POST.get('address')
+        phone   = request.POST.get('phone')
+        pincode = request.POST.get('pincode')
+        cart    = request.session.get('cart')
+        uid     = request.session.get('_auth_user_id')
+        user    = User.objects.get(pk = uid)
+
+        for i in cart:
+            price       = (int(cart[i]['price']))
+            quantity    = cart[i]['quantity']
+            total       = price * quantity
+
+            order = Order(
+                user    = user,
+                product = cart[i]['name'],
+                price   = cart[i]['price'],
+                quantity= cart[i]['quantity'],
+                image   = cart[i]['image'],
+                address = address,
+                phone   = phone,
+                pincode = pincode,
+                total   = total,
+            )
+            order.save()
+        request.session['cart'] = {}
+        return redirect('index')
+        
+    return HttpResponse('this is checkout page')
+
+def order_page(request):
+    uid     = request.session.get('_auth_user_id')
+    user    = User.objects.get(pk = uid)
+
+    order   = Order.objects.filter(user = user)
+    context = {
+        'order' : order,
+    }
+    return render(request,'order.html', context)
+
+def product_page(request):
+    category   = Category.objects.all()
+    brand      = Brand.objects.all()
+
+    categoryID = request.GET.get('category')
+    brandID    = request.GET.get('brand')
+
+    if categoryID:
+        product     = Product.objects.filter(sub_Category=categoryID).order_by('-id')
+    elif brandID:
+        product     = Product.objects.filter(brand=brandID).order_by('-id')
+    else:
+        product     = Product.objects.all() 
+
+    context = {
+        'category': category,
+        'product' : product,
+        'brand'   : brand,  
+    }
+    return render(request, 'product.html', context)
+
+def product_detail_page(request,id):
+    product = Product.objects.filter(id = id).first()
+    context = {
+        'product':product,
+    }
+    return render(request,'product_detail.html', context)
+
+def search_page(request):
+    search  = request.GET['search']
+    product = Product.objects.filter(name__icontains = search)
+    context = {
+        'product':product,
+    }
+    return render(request, 'search.html',context)
